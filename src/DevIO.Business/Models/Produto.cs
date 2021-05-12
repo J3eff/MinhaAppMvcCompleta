bfc80ddb7c6eebe162aa90b60601126e4930cf3a
J3eff <jeffersonvideo125@gmail.com>
2021-05-12 06:49:04 -0300

    initial commit

---
+++ src/DevIO.Business/Models/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevIO.Business.Models
+{
+    public class Produto
+    {
+        public Guid FornecedorId { get; set; }
+
+        public string Nome { get; set; }
+        public string Descrica { get; set; }
+        public string Imagem { get; set; }
+        public string Valor { get; set; }
+        public bool DataCadastro { get; set; }
+
+        /*EF Relation*/
+        public Fornecedor Fornecedor { get; set; }
+    }
+}
