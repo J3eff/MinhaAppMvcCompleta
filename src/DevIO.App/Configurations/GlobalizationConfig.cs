2425dac082dd1950c1fb51cb3bcdcb8b1025148a
J3eff <jeffersonvideo125@gmail.com>
2021-05-27 19:16:14 -0300

    Organizando e otimizando a estrutura da App

---
+++ src/DevIO.App/Configurations/GlobalizationConfig.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Localization;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace DevIO.App.Configurations
+{
+    public static class GlobalizationConfig
+    {
+        public static IApplicationBuilder UseGlobalizationConfig(this IApplicationBuilder app)
+        {
+            //Configuração da linguagem da aplicação
+            var defaultCulture = new CultureInfo("pt-BR");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(defaultCulture),
+                SupportedCultures = new List<CultureInfo> { defaultCulture },
+                SupportedUICultures = new List<CultureInfo> { defaultCulture }
+            };
+            app.UseRequestLocalization(localizationOptions);
+
+            return app;
+        }
+    }
+
+}
