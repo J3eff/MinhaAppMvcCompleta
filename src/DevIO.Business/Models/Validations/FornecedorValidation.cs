0aff6af61a1461978adfa0b86b7d5479e02a872e
J3eff <jeffersonvideo125@gmail.com>
2021-05-31 22:03:56 -0300

    Validando as entidades de negocio

---
+++ src/DevIO.Business/Models/Validations/FornecedorValidation.cs
@@ -0,0 +1,34 @@
+﻿using DevIO.Business.Models.Validations.Documentos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevIO.Business.Models.Validations
+{
+    public class FornecedorValidation : AbstractValidator<Fornecedor>
+    {
+        public FornecedorValidation()
+        {
+            RuleFor(f => f.Nome)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .Length(2, 100) //Indica o minimo e maximo do nome fornecido.
+                .WithMessage("O campo {PropertyName} precisa ter entre {MinLegth} e {MaxLength} caracteres");
+
+            When(f => f.TipoFornecedor == TipoFornecedor.PessoaFisica, () => 
+            {
+                RuleFor(f => f.Documento.Length).Equal(CpfValidacao.TamanhoCpf)
+                .WithMessage("O campo Documento precisa ter {ComparisonValue} caracteres e foi fornecido {PropertyValue}.");
+                RuleFor(f => CpfValidacao.Validar(f.Documento)).Equal(true)
+                    .WithMessage("O documento fornecido é invalido.");
+            });
+
+            When(f => f.TipoFornecedor == TipoFornecedor.PessoaJurifica, () => {
+                RuleFor(f => f.Documento.Length).Equal(CnpjValidacao.TamanhoCnpj)
+                .WithMessage("O campo Documento precisa ter {ComparisonValue} caracteres e foi fornecido {PropertyValue}.");
+                RuleFor(f => CpfValidacao.Validar(f.Documento)).Equal(true)
+                    .WithMessage("O documento fornecido é invalido.");
+            });
+        }
+    }
+}
