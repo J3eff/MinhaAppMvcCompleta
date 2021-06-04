aa785b3ca85276be2d36766fd394cbd1e615054f
J3eff <jeffersonvideo125@gmail.com>
2021-06-04 12:29:53 -0300

    Seguranca e tecnicas ineditas com Identity

---
+++ src/DevIO.App/Extensions/ApagaElementoByClaimTagHelper.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.App.Extensions
+{
+    [HtmlTargetElement("*", Attributes = "surpress-by-claim-name")]
+    [HtmlTargetElement("*", Attributes = "surpress-by-claim-value")]
+    public class ApagaElementoByClaimTagHelper : TagHelper
+    {
+        private readonly IHttpContextAccessor _contextAcessor;
+
+        public ApagaElementoByClaimTagHelper(IHttpContextAccessor contextAcessor)
+        {
+            _contextAcessor = contextAcessor;
+        }
+
+        [HtmlAttributeName("surpress-by-claim-name")]
+        public string IdentityClaimName { get; set; }
+
+        [HtmlAttributeName("surpress-by-claim-value")]
+        public string IdentityClaimValue { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            if (output == null)
+                throw new ArgumentNullException(nameof(output));
+
+
+            var temAcesso = CustomAuthorization.ValidarClaimsUsuario(_contextAcessor.HttpContext, IdentityClaimName, IdentityClaimValue);
+
+            if (temAcesso) return;
+
+            output.SuppressOutput();
+        }
+    }
+
+    [HtmlTargetElement("a", Attributes = "disable-by-claim-name")]
+    [HtmlTargetElement("a", Attributes = "disable-by-claim-value")]
+    public class DesabilitaLinkClaimTagHelper : TagHelper
+    {
+        private readonly IHttpContextAccessor _contextAcessor;
+
+        public DesabilitaLinkClaimTagHelper(IHttpContextAccessor contextAcessor)
+        {
+            _contextAcessor = contextAcessor;
+        }
+
+        [HtmlAttributeName("disable-by-claim-name")]
+        public string IdentityClaimName { get; set; }
+
+        [HtmlAttributeName("disable-by-claim-value")]
+        public string IdentityClaimValue { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            if (output == null)
+                throw new ArgumentNullException(nameof(output));
+
+            var temAcesso = CustomAuthorization.ValidarClaimsUsuario(_contextAcessor.HttpContext, IdentityClaimName, IdentityClaimValue);
+
+            if (temAcesso) return;
+
+            output.Attributes.RemoveAll("href");
+            output.Attributes.Add(new TagHelperAttribute("style", "cursor: not-allowed"));
+            output.Attributes.Add(new TagHelperAttribute("title", "Você não tem permissão")); 
+        }
+    }
+
+    [HtmlTargetElement("*", Attributes = "supress-by-action")]
+    public class ApagarElementoByActionTagHelper : TagHelper
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public ApagarElementoByActionTagHelper(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
+        [HtmlAttributeName("supress-by-action")]
+        public string ActioName { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            if (output == null)
+                throw new ArgumentNullException(nameof(output));
+
+            //Ele pega uma coleção de dados da rota, atravez da chave ACTION, desta forma conseguimos pegar rota da action dentro do Request
+            var action = _contextAccessor.HttpContext.GetRouteData().Values["action"].ToString(); 
+
+            if (ActioName.Contains(action)) return;
+
+            output.SuppressOutput();
+        }
+
+    } 
+}
